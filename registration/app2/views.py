48fbe406ccf43d9f92e6b5c35513cf3845a7d2a4
Zulaikha <zbintyafzal@gmail.com>
2024-03-23 21:36:13 +0500

    All files

---
+++ registration/app2/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render ,HttpResponse, redirect
+from requests import request
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+
+# Create your views here.
+def HomePage(request):
+    return render(request,'home.html')
+def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        password1=request.POST.get('password1')
+        password2=request.POST.get('password2')
+        
+        if password1!=password2:
+            return HttpResponse('Your password is not same')
+        else:                    
+            my_user=User.objects.create_user(uname,email,password1)
+            my_user.save()
+            return redirect('login')
+        
+    
+    return render(request,'signup.html')
+
+def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse('Incorect usernamee or password!!')
+        
+    return render(request,'login.html')
